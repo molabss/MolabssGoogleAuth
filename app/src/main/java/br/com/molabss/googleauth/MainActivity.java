a67f7aefe6646007c581da9dc369dfb9a249e733
molabss@gmail.com <molabss@gmail.com>
2017-07-08 18:30:38 -0300

    Initial commit

---
+++ app/src/main/java/br/com/molabss/googleauth/MainActivity.java
@@ -0,0 +1,39 @@
+package br.com.molabss.googleauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnSigninGoogle)Button btnSigninGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @OnClick(R.id.btnSigninGoogle)
+    public void onClick(View v){
+        if(v.getId() == R.id.btnSigninGoogle){
+
+        }
+    }
+}
