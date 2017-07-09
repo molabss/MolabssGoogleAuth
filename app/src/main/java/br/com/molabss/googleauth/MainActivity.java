37cd3983de998b835f3afed6f5d305374dcc7247
molabss@gmail.com <molabss@gmail.com>
2017-07-09 16:38:06 -0300

    Adicionado Sing out

--- app/src/main/java/br/com/molabss/googleauth/MainActivity.java
+++ app/src/main/java/br/com/molabss/googleauth/MainActivity.java
@@ -1,9 +1,22 @@
 package br.com.molabss.googleauth;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,13 +24,38 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btnSigninGoogle)Button btnSigninGoogle;
+    @BindView(R.id.btnSigninGoogle)SignInButton btnSigninGoogle;
+    @BindView(R.id.btnSignOut)Button btnSignOut;
+    @BindView(R.id.mStatusTextView)TextView mStatusTextView;
+    private static final int RC_SIGN_IN = 9001;
+    GoogleApiClient mGoogleApiClient;
+    String accountName = "";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+            }
+        })
+        .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+        .build();
+
     }
 
     @Override
@@ -30,10 +68,57 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    @OnClick(R.id.btnSigninGoogle)
-    public void onClick(View v){
-        if(v.getId() == R.id.btnSigninGoogle){
 
+    @OnClick(R.id.btnSigninGoogle)
+    public void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @OnClick(R.id.btnSignOut)
+    public void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+        new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                if(status.isSuccess()){
+                    mStatusTextView.setText(getString(R.string.sign_out));
+                }else {
+                    updateUI(false);
+                }
+            }
+        });
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            accountName =  acct.getDisplayName();
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+    public void updateUI(boolean update){
+        if (update == true){
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, accountName));
+        } else {
+            mStatusTextView.setText(getString(R.string.sign_out));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
         }
     }
 }
